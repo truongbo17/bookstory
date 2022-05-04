b2d9569d3901bcc8897aa0a5567b4a7cb0553eca
Nguyen Quang Truong <truongnq017@gmail.com>
2022-05-04 15:43:22 +0700

    hot fix

--- database/migrations/2022_04_23_064201_add_unique_slug_to_table.php
+++ database/migrations/2022_04_23_064201_add_unique_slug_to_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('slug')->nullable(false)->change();
             $table->unique('slug');
         });
-        Schema::table('categories', function (Blueprint $table) {
-            $table->unique('slug');
-        });
     }
 
     /**
