b3679d9537876ce407ebf99c7f440017b1e18b2f
Nguyen Quang Truong <truongnq017@gmail.com>
2022-05-10 21:58:49 +0700

    hot fix

--- database/migrations/2022_05_07_064190_create_reviews_table.php
+++ database/migrations/2022_05_07_064190_create_reviews_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('reviews');
         Schema::create('reviews', function (Blueprint $table) {
                 $table->id();
                 $table->unsignedBigInteger('document_id');
