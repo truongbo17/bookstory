7bf944ed96ab43522ef6c4364f2832d6c3e1bfcb
Nguyen Quang Truong <truongnq017@gmail.com>
2022-05-24 22:47:03 +0700

    language meta

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,13 +8,13 @@
 @push('seo')
     <meta name="description"
           content="{{__('Bookstory - The free ebook library has more than 5000 titles to read online, read stories online, download stories, download books for free. The free bookstore comes in a variety of formats to read on a variety of devices.')}}">
-    <meta name="keywords" content="{{__('pdf free download')}}">
+    <meta name="keywords" content="pdf free download,free upload docment,bookstory,pdf free reading online">
+    <meta property="og:image" content="{{asset('images/preview/image-preview.png')}}"/>
     <meta property="og:type" content="books.book"/>
     <meta property="og:description"
           content="{{__('Bookstory - The free ebook library has more than 5000 titles to read online, read stories online, download stories, download books for free. The free bookstore comes in a variety of formats to read on a variety of devices.')}}"/>
-    <meta property="og:image" content="{{asset('images/preview/image-preview.png')}}"/>
     <meta property="og:url" content="{{env('APP_URL')}}"/>
-    <meta property="og:locale" content="{{__('locale')}}" />
+    <meta property="og:locale" content="{{__('locale')}}"/>
 @endpush
 
 @section('message')
