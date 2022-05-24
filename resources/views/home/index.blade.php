96c56fa73e2f4294ffde7e56b4e6b311ea1ff79e
Nguyen Quang Truong <truongnq017@gmail.com>
2022-05-24 22:24:45 +0700

    seo tag

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,6 +5,18 @@
 @push('css')
 @endpush
 
+@push('seo')
+    <meta name="description"
+          content="{{__('Bookstory - The free ebook library has more than 5000 titles to read online, read stories online, download stories, download books for free. The free bookstore comes in a variety of formats to read on a variety of devices.')}}">
+    <meta name="keywords" content="{{__('pdf free download')}}">
+    <meta property="og:type" content="books.book"/>
+    <meta property="og:description"
+          content="{{__('Bookstory - The free ebook library has more than 5000 titles to read online, read stories online, download stories, download books for free. The free bookstore comes in a variety of formats to read on a variety of devices.')}}"/>
+    <meta property="og:image" content="{{asset('images/preview/image-preview.png')}}"/>
+    <meta property="og:url" content="{{env('APP_URL')}}"/>
+    <meta property="og:locale" content="{{__('locale')}}" />
+@endpush
+
 @section('message')
     @include('layouts.message')
 @endsection
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,20 +2,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookStory - @yield('title')</title>
-    <meta name="google-site-verification" content="C9aBycPGfUFFJ-UPegHJ2970JmiJH22WbeEEuuCU8cM" />
+    <meta name="google-site-verification" content="C9aBycPGfUFFJ-UPegHJ2970JmiJH22WbeEEuuCU8cM"/>
     <meta name=”robots” content=”all”/>
     <meta http-equiv=”content-language” content=”{{__('lang')}}”/>
-    @stack('seo','<meta name="description" content="Bookstory it is contains Great selection of modern and classic books waiting to be discovered. All free and available in PDF Files.">
-    <meta name="keywords" content="bookstory,tai liệu miễn phí,tai lieu mien phi,PDF Free Download,tải lên tài liệu,upload free">
-')
-    <link rel="icon" type="image/png" href="{{ asset('') }}">
+    @stack('seo')
+    <link rel="icon" type="image/png" href="{{ asset('images/logo/logo-header.png') }}">
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.2/dist/flowbite.min.css"/>
     <link href="{{ asset('1/app.css') }}" rel="stylesheet">
     @stack('css')
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XCKX81Y594"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'G-XCKX81Y594');
