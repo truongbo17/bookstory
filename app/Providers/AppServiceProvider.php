0549676383fe5406d66a2b3a7325ad9eec8682e2
Nguyen Quang Truong <truongnq017@gmail.com>
2022-06-03 23:04:46 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,11 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // \URL::forceRootUrl(Config::get('app.url'));
+        \URL::forceRootUrl(Config::get('app.url'));
 
-        // if (Str::contains(Config::get('app.url'), 'https://')) {
-        //     \URL::forceScheme('https');
-        // }
+        if (Str::contains(Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+        }
 
         Storage::disk(config('crawl.pdf_disk'))->buildTemporaryUrlsUsing(function ($path, $expiration, $options) {
             return URL::temporarySignedRoute(
